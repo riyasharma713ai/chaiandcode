fe1ee61a0a8200e5f8a31647125f4d542841ab8b
riyasharma713ai <rs3875258@gmail.com>
2024-06-03 16:15:38 +0530

    pushing lec6

---
+++ lec6.js
@@ -0,0 +1,16 @@
+// console.log("Riya");
+// let score = "31";
+// console.log(typeof score)
+// let convert = Number(score);
+// console.log(typeof convert);
+// console.log(convert);
+
+// let score = "abcd";
+// console.log(typeof score);
+// let d="d";
+// console.log(boo);
+let somenumber=67
+
+let string=String(somenumber);
+console.log(string);
+console.log( typeof string);
