bd36c2844c17d602bcbc1e5f2cf83e9dddeee57b
riyasharma713ai <rs3875258@gmail.com>
2024-06-03 10:53:27 +0530

    first commit

---
+++ lec5..js
@@ -0,0 +1,18 @@
+console.log("Riya");
+let age=18;
+let isloggedin=true
+let state=null
+
+// number => 2 to the power 53
+// BigInt=
+// String=>""
+// Boolean=>true/false
+// null => standalone value
+// undefined =>
+    
+console.log(typeof undefined);
+console.log(typeof null);
+
+
+
+
