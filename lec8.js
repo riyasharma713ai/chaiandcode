0abf00e758398abab6e2125bcd3ee128f8edc26e
riyasharma713ai <rs3875258@gmail.com>
2024-06-04 14:43:17 +0530

    Pushing lecture8

---
+++ lec8.js
@@ -0,0 +1,16 @@
+ console.log("riya");
+console.log(2>1);
+console.log(2>=1);
+console.log(5>=4);
+console.log(7==8);
+console.log(9<=8);
+// Whatever is the first sign java will only address only that the second sign will not be addressed by java..for eg:(5>=4) the answer will be true because it is readind only the first coomand or sign which is greater than ">"the second sign is not being read by java as it do not read the socond command or sign which is "=".
+ console.log(null> 0);
+ console.log(null== 0);
+ console.log(null>=0);
+ console.log(undefined>=0);
+ console.log(undefined==0);
+ console.log(undefined<=0);
+ console.log(2=="2");
+  console.log(2==="2");
+ //here what happens is the java checks its strictly as in it we use ""for string and for any other we dont go for it such that when we use java here === triple= it check every single detail strictly as in "2"and 2 has a difference in their datatype "2"is a string and 2 is a number so it say that it is not equaland gives the output as false.
