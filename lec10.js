be9460f08346deb8b50e2cc46809bc5711dd5b69
riyasharma713ai <rs3875258@gmail.com>
2024-06-07 11:18:38 +0530

    ist push

---
+++ lec10.js
@@ -0,0 +1,9 @@
+console.log("Riya");
+const name= "Riya"
+const age=21
+//console.log(name+age+ value);
+//THis technique should not be used as it is not that worthit it seems that we are not that experienced or a low category developer..
+
+//WE USE BACKTIKS(``)WHILE INJECTING  A VARIABLE IN A STRING AND WE USE $()DOLLAR AND PARENTHESIS WHILE INJECTING VARIABLE.
+
+console.log(`Hello my name is ${name} and my age is ${age}`);
