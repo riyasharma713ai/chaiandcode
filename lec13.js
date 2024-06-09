705d9ea8c3d89aa9804afcfe3b0ee2e467df44bc
riyasharma713ai <rs3875258@gmail.com>
2024-06-10 01:55:03 +0530

    pushing lec13

---
+++ lec13.js
@@ -0,0 +1,15 @@
+console.log("Riya");
+const min=10
+const max=20
+
+console.log(Math.floor(Math.random()*(max-min+1))+min)
+console.log(Math.random());
+
+console.log((Math.random()*10)+1);
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.ceil(4.32));
+console.log(Math.floor(4.9));
+console.log(Math.max(2,3,4,5));
+console.log(Math.min(8,9,7,5));
