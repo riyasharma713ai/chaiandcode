390de5848d3f90bd6c134c5bbfd377f7ea568244
riyasharma713ai <rs3875258@gmail.com>
2024-06-10 01:05:10 +0530

    pushing lec12

---
+++ lec12.js
@@ -0,0 +1,12 @@
+console.log("Riya");
+const score=400
+const balance=new Number(100)
+console.log(balance);
+console.log(balance.toString());
+console.log(balance.toString().length);
+console.log(balance.toFixed(1));
+const otherNumber=1123.8574
+console.log(otherNumber.toPrecision(3));
+
+const hundreds=10000000
+console.log(hundreds.toLocaleString('en-IN'));
