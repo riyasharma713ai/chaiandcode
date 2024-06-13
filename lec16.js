ffeb5def5245c61c8a2808798c3366804a75686b
riyasharma713ai <rs3875258@gmail.com>
2024-06-13 22:53:20 +0530

    pushing lec16

---
+++ lec16.js
@@ -0,0 +1,69 @@
+console.log("Riya");
+const myArr =[1,2,3,4,5]
+console.log(myArr[0]);
+
+  
+
+const myHeroes =[true,"ronak"]
+console.log(myHeroes[0]);
+const myArr2 =new Array(1,2,3,4)
+console.log(myArr[0]);
+
+
+
+//Array methods
+console.log(myArr);
+myArr.push("riya","pushpa")
+console.log(myArr);
+//push it just adds any no. sytring or anything which ever we wanyt to add
+
+myArr.pop()
+console.log(myArr);
+//pop removes the last data of the array
+
+myArr.unshift(0)
+console.log(myArr);
+//unshift adds the data at the first that's it 
+
+myArr.shift()
+console.log(myArr); 
+//shift it just remove the data from the first that's it
+
+
+console.log(myArr.includes("rohit"));
+//includes stands for giving result either the data is present or not
+
+console.log(myArr.indexOf(8));
+//indexof stands for letting know either the data is present or not -1 stands for not knowing either it is present or not and normal no. gives the index no.
+
+const myArr3 =myArr.join()
+console.log(myArr3);
+console.log( typeof myArr3);
+//join just changes the array into string
+
+console.log("A",myArr);
+const myNewArr= myArr.slice(1,3)
+console.log(myNewArr);
+const myNEWArr1= myArr.slice(2,3)
+console.log(myNEWArr1);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
