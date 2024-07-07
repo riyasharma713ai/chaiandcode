b543ba4bc17b76349831fdc8a365be95eccb50a0
riyasharma713ai <rs3875258@gmail.com>
2024-07-07 15:56:56 +0530

    pushing lec21.js

--- lec21.js
+++ lec21.js
@@ -13,8 +13,10 @@ function addtwonumbers(number1,number2){
     //console.log(number1+number2);
 return result
 //return k badh kuch v print nahi hota ..
+console.log("riya");//this won't print..
 }
  const result=addtwonumbers(3,4)
 
 
 console.log("Result:",result);
+console.log("hitesh");
