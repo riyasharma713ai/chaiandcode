b138b4561367a38b3ca49c992d73753d9b5350f5
riyasharma713ai <rs3875258@gmail.com>
2024-07-08 15:58:22 +0530

    pushing lec21.js

--- lec21.js
+++ lec21.js
@@ -1,22 +1,43 @@
 console.log("Functions");
 
 function saymyname(){
-    console.log("R");
-    console.log("I");
-    console.log("Y");
-    console.log("A");
 
+console.log("R");
+console.log("I");
+console.log("Y");
+console.log("A ");
 }
 saymyname()
-function addtwonumbers(number1,number2){
-    let result=number1+number2
-    //console.log(number1+number2);
-return result
-//return k badh kuch v print nahi hota ..
-console.log("riya");//this won't print..
+saymyname()
+saymyname()
+saymyname()
+// function addtwonumbers(number1,number2){
+//     let result=number1+number2
+//     console.log(number1+number2);
+
+// return result}
+
+// //return k badh kuch v print nahi hota ..
+// console.log("riya");//this won't print..
+// }
+//  const result=addtwonumbers(3,4)
+
+
+// console.log("Result:",result);
+// console.log("hitesh");
+// }
+// const addition=number1+number2;
+
+
+
+
+
+function question(number1 , number2 , number3) {
+ const riya=number1 + number2 + number3
+ console.log(riya);
+
 }
- const result=addtwonumbers(3,4)
 
+question(31 , 32 , 44);
+question(44 , 50 , 90);
 
-console.log("Result:",result);
-console.log("hitesh");
